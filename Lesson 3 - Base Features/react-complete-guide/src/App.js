23a266dc8fdbc788483686c53f1d8855593d3c0e
Craig Miller <craigmiller160@gmail.com>
2018-06-16 18:31:22 -0400

    Added more behavior and state to components

--- Lesson 3 - Base Features/react-complete-guide/src/App.js
+++ Lesson 3 - Base Features/react-complete-guide/src/App.js
@@ -1,16 +1,73 @@
 import React, { Component } from 'react';
 import './App.css';
-import Person from './Person/Person';
+import Person from './Person/Person'
+
+const people = [
+    {
+        name: 'Max',
+        age: 28,
+        children: null
+    },
+    {
+        name: 'Manu',
+        age: 29,
+        children: 'My Hobbies: Racing'
+    },
+    {
+        name: 'Stephanie',
+        age: 26,
+        children: null
+    }
+];
 
 export default class App extends Component {
+    state = {
+        people,
+        otherState: 'some other value'
+    };
+
+    // noinspection JSMethodCanBeStatic
+    switchNameHandler = (newName) => {
+        // DON'T DO THIS: this.state.people[0].name = 'Maximilian';
+        const newPeople = people.slice();
+        newPeople[0].name = newName;
+        this.setState({
+            people: newPeople
+        });
+    };
+
+    nameChangedHandler = (event) => {
+        const newPeople = people.slice();
+        newPeople[1].name = event.target.value;
+        this.setState({
+            people: newPeople
+        });
+    };
+
     render() {
         return (
             <div className="App">
                 <h1>Hi, I'm a React App</h1>
                 <p>This is really working</p>
-                <Person name="Max" age="28" />
-                <Person name="Manu" age="29">My Hobbies: Racing</Person>
-                <Person name="Stephanie" age="26" />
+                <button onClick={() => this.switchNameHandler('Maximilian')}>Switch Name</button>
+                <Person
+                    click={this.switchNameHandler.bind(this, 'Max!')}
+                    change={this.nameChangedHandler}
+                    name={this.state.people[0].name}
+                    age={this.state.people[0].age} />
+                <Person
+                    click={this.switchNameHandler.bind(this, 'Max!')}
+                    change={this.nameChangedHandler}
+                    name={this.state.people[1].name}
+                    age={this.state.people[1].age}>
+                    My Hobbies: Racing
+                </Person>
+                <Person
+                    click={this.switchNameHandler.bind(this, 'Max!')}
+                    change={this.nameChangedHandler}
+                    name={this.state.people[2].name}
+                    age={this.state.people[2].age} />
+                <p>{this.state.otherState}</p>
             </div>
         );
     //   return React.createElement('div', {className: 'App'},
--- Lesson 3 - Base Features/react-complete-guide/src/Person/Person.js
+++ Lesson 3 - Base Features/react-complete-guide/src/Person/Person.js
@@ -3,8 +3,9 @@ import React from 'react';
 export default props => {
     return (
         <div>
-            <p>I'm {props.name} and I am {props.age} years old!</p>
+            <p onClick={props.click}>I'm {props.name} and I am {props.age} years old!</p>
             <p>{props.children}</p>
+            <input type="text" onChange={props.change} value={props.name} />
         </div>
     );
 }
