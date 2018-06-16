49969d212c91a507df1fc9ee5308ad516afc88ee
Craig Miller <craigmiller160@gmail.com>
2018-06-16 16:58:50 -0400

    Added handling for props children

--- Lesson 3 - Base Features/react-complete-guide/src/Person/Person.js
+++ Lesson 3 - Base Features/react-complete-guide/src/Person/Person.js
@@ -2,6 +2,9 @@ import React from 'react';
 
 export default props => {
     return (
-        <p>I'm {props.name} and I am {props.age} years old!</p>
+        <div>
+            <p>I'm {props.name} and I am {props.age} years old!</p>
+            <p>{props.children}</p>
+        </div>
     );
 }
