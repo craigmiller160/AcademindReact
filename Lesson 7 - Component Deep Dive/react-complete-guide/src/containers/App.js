8c11a35b379e499a4244407a3117e0ae668af08b
Craig Miller <craigmiller160@gmail.com>
2018-06-17 17:24:29 -0400

    Added props to a stateful component

--- Lesson 7 - Component Deep Dive/react-complete-guide/src/containers/App.js
+++ Lesson 7 - Component Deep Dive/react-complete-guide/src/containers/App.js
@@ -42,6 +42,7 @@ class App extends Component {
         return (
             <div className={classes.App}>
                 <Cockpit
+                    title={this.props.title}
                     peopleLength={this.state.people.length}
                     showPeople={this.state.showPeople}
                     togglePeople={this.togglePersonsHandler} />
