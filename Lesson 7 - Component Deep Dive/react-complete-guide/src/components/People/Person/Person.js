fa9abeb253772f94b81491fd8e09351ac6646e38
Craig Miller <craigmiller160@gmail.com>
2018-06-17 17:17:33 -0400

    Removed the extra P tag for the children of Person

--- Lesson 7 - Component Deep Dive/react-complete-guide/src/components/People/Person/Person.js
+++ Lesson 7 - Component Deep Dive/react-complete-guide/src/components/People/Person/Person.js
@@ -1,11 +1,18 @@
 import React from 'react';
 import classes from './Person.css';
 
+//Just a helper function
+const hasChildren = props => {
+    return props.children != null && props.children.length > 0;
+};
+
 const Person = props => {
+    const children = hasChildren(props) > 0 ? <p>{props.children}</p> : null;
+
     return (
         <div className={classes.Person}>
             <p onClick={props.click}>I'm {props.name} and I am {props.age} years old!</p>
-            <p>{props.children}</p>
+            {children}
             <input type="text" onChange={props.change} value={props.name} />
         </div>
     );
