fa26ab0bc618d2f1a2935e125d72e8d317c1ebc7
craigmiller160 <craigmiller160@gmail.com>
2018-06-26 18:35:40 -0400

    Added prop types to the lesson

--- Lesson 7 - Component Deep Dive/react-complete-guide-lifecycle/src/components/People/Person/Person.js
+++ Lesson 7 - Component Deep Dive/react-complete-guide-lifecycle/src/components/People/Person/Person.js
@@ -1,6 +1,7 @@
 import React, { Component, Fragment } from 'react';
 import classes from './Person.css';
 import withClass from '../../../hoc/withClass';
+import PropTypes from 'prop-types';
 
 class Person extends Component {
 
@@ -40,5 +41,12 @@ class Person extends Component {
     }
 
 }
+
+Person.propTypes = {
+    name: PropTypes.string,
+    age: PropTypes.number,
+    click: PropTypes.func,
+    change: PropTypes.func
+};
 
 export default withClass(Person, classes.Person);
