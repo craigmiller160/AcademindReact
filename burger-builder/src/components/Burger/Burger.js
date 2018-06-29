07eedf9764d6a5bb17b3dfdb8b36af098a5eb981
Craig Miller <craigmiller160@gmail.com>
2018-06-29 08:06:28 -0400

    Made the Burger dynamic based on props from the BurgerBuilder

--- burger-builder/src/components/Burger/Burger.js
+++ burger-builder/src/components/Burger/Burger.js
@@ -1,16 +1,31 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import classes from './Burger.css';
 import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
 
 const Burger = props => {
+    const ingredients = Object.keys(props.ingredients)
+        .map(key => {
+            return [...Array(props.ingredients[key])]
+                .map((_,i) => <BurgerIngredient type={key} key={key + i} />)
+        });
+
     return (
         <div className={classes.Burger}>
             <BurgerIngredient type="bread-top"/>
-            <BurgerIngredient type="cheese"/>
-            <BurgerIngredient type="meat"/>
+            {ingredients}
             <BurgerIngredient type="bread-bottom"/>
         </div>
     );
+};
+
+Burger.propTypes = {
+    ingredients: PropTypes.shape({
+        salad: PropTypes.number,
+        bacon: PropTypes.number,
+        cheese: PropTypes.number,
+        meat: PropTypes.number
+    }).isRequired
 };
 
 export default Burger;
