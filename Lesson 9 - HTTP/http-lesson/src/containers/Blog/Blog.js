64dc453dcad1e1314b509148428334eebaa28cfa
Craig Miller <craigmiller160@gmail.com>
2018-07-04 10:57:41 -0400

    Added the posts to the UI

--- Lesson 9 - HTTP/http-lesson/src/containers/Blog/Blog.js
+++ Lesson 9 - HTTP/http-lesson/src/containers/Blog/Blog.js
@@ -1,18 +1,35 @@
 import React, { Component } from 'react';
-
+import axios from 'axios';
 import Post from '../../components/Post/Post';
 import FullPost from '../../components/FullPost/FullPost';
 import NewPost from '../../components/NewPost/NewPost';
 import './Blog.css';
 
 class Blog extends Component {
+
+    state = {
+        posts: []
+    };
+
+    async componentDidMount() {
+        try {
+            const res = await axios.get('https://jsonplaceholder.typicode.com/posts');
+            this.setState({posts: res.data});
+        }
+        catch (ex) {
+            console.log(ex);
+        }
+    }
+
     render () {
+        const postElems = this.state.posts.map(post => (
+            <Post key={post.id} title={post.title} />
+        ));
+
         return (
             <div>
                 <section className="Posts">
-                    <Post />
-                    <Post />
-                    <Post />
+                    {postElems}
                 </section>
                 <section>
                     <FullPost />
