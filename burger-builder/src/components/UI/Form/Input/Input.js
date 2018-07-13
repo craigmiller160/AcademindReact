c59947e47c2ff1e81d1091344c072eb65bb43677
Craig Miller <craigmiller160@gmail.com>
2018-07-12 21:55:54 -0400

    Created custom input component

---
+++ burger-builder/src/components/UI/Form/Input/Input.js
@@ -0,0 +1,48 @@
+import React, { Component } from 'react';
+import classes from './Input.css';
+import PropTypes from 'prop-types';
+import nextId from '../../../../util/nextId';
+
+class Input extends Component {
+
+    state = {
+        id: 0
+    };
+
+    componentDidMount() {
+        this.setState({id: nextId()});
+    }
+
+    render() {
+        return (
+            <div className={classes.Input}>
+                <label htmlFor={this.state.id}>{this.props.label}: </label>
+                <input
+                    id={this.state.id}
+                    className={this.props.className}
+                    type={this.props.type}
+                    name={this.props.name}
+                    placeholder={this.props.placeholder}
+                    value={this.props.value}
+                    onChange={this.props.change} />
+            </div>
+        );
+    }
+
+}
+
+Input.propTypes = {
+    type: PropTypes.string,
+    name: PropTypes.string,
+    label: PropTypes.string.isRequired,
+    placeholder: PropTypes.string,
+    value: PropTypes.any,
+    change: PropTypes.func,
+    className: PropTypes.string
+};
+
+Input.defaultProps = {
+    type: 'text'
+};
+
+export default Input;
--- burger-builder/src/containers/Checkout/ContactData/ContactData.js
+++ burger-builder/src/containers/Checkout/ContactData/ContactData.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import classes from './ContactData.css';
 import Button from '../../../components/UI/Button/Button';
+import Input from '../../../components/UI/Form/Input/Input';
 
 class ContactData extends Component {
 
@@ -22,10 +23,10 @@ class ContactData extends Component {
             <div className={classes.ContactData}>
                 <h4>Enter your Contact Data</h4>
                 <form>
-                    <input type="text" name="name" placeholder="Your name" />
-                    <input type="email" name="email" placeholder="Your email" />
-                    <input type="text" name="street" placeholder="Your street" />
-                    <input type="text" name="postalCode" placeholder="Your postal code" />
+                    <Input label="Name" type="text" name="name" placeholder="Your name" />
+                    <Input label="Email" type="email" name="email" placeholder="Your email" />
+                    <Input label="Street" type="text" name="street" placeholder="Your street" />
+                    <Input label="Postal Code" type="text" name="postalCode" placeholder="Your postal code" />
                     <Button btnType="Success" clicked={this.orderBurgerHandler}>ORDER</Button>
                 </form>
             </div>
