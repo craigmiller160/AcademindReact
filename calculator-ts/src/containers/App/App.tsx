c0b9eb1f7da2ba17348d4e6e9cf3ff19b66291f6
craigmiller160 <craigmiller160@gmail.com>
2018-07-16 15:44:31 -0400

    Added default 0 placeholder value

--- calculator-ts/src/containers/App/App.tsx
+++ calculator-ts/src/containers/App/App.tsx
@@ -8,29 +8,34 @@ import calculate from '../../util/calculate';
 class App extends Component<{},IAppState> {
 
     public state = {
-        displayValue: ''
+        displayValue: '0'
     };
 
     public buttonClickHandler = (type: string): void => {
         this.setState(prevState => {
             try {
+                const oldValue = prevState.displayValue === '0' ? '' : prevState.displayValue;
                 let newValue = '';
                 switch (type) {
                     case '+':
                     case '-':
                     case '*':
                     case '/':
-                        newValue = `${prevState.displayValue} ${type} `;
+                        newValue = `${oldValue} ${type} `;
                         break;
                     case '=':
-                        newValue = calculate(prevState.displayValue);
+                        newValue = calculate(oldValue);
                         break;
                     case 'Clear':
                         newValue = '';
                         break;
                     default:
-                        newValue = `${prevState.displayValue}${type}`;
+                        newValue = `${oldValue}${type}`;
                         break;
+                }
+
+                if (newValue === '') {
+                    newValue = '0';
                 }
 
                 return {displayValue: newValue};
