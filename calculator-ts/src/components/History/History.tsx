9d7501d762b49f06de4ad4b2229ea05505e36490
craigmiller160 <craigmiller160@gmail.com>
2018-07-16 16:58:45 -0500

    Reversed the order of the history

--- calculator-ts/src/components/History/History.tsx
+++ calculator-ts/src/components/History/History.tsx
@@ -3,11 +3,12 @@ import classes from './History.css';
 import IHistoryProps from './IHistoryProps';
 
 const History: SFC<IHistoryProps> = props => {
+    const history = props.history.reverse();
     return (
         <div className={classes.History}>
             <h3>History</h3>
             <div className={classes.list}>
-                {props.history.map((item, index) => <p key={index}>{item}</p>)}
+                {history.map((item, index) => <p key={index}>{item}</p>)}
             </div>
         </div>
     );
