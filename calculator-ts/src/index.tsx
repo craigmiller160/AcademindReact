c44475e5f1f189b2df425c49db3e353fc3de8eeb
craigmiller160 <craigmiller160@gmail.com>
2018-07-16 15:39:48 -0400

    Slightly cleaned up imports

--- calculator-ts/src/index.tsx
+++ calculator-ts/src/index.tsx
@@ -1,5 +1,5 @@
-import * as React from 'react';
-import * as ReactDOM from 'react-dom';
+import React from 'react';
+import ReactDOM from 'react-dom';
 import App from './containers/App/App';
 import './index.css';
 import registerServiceWorker from './registerServiceWorker';
