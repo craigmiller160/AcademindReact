cd1f7774fc65d45f61d12d7a6a9bdce99e7f545d
craigmiller160 <craigmiller160@gmail.com>
2018-07-16 13:37:13 -0400

    Started work on Orders page

--- burger-builder/src/http/axios-orders.js
+++ burger-builder/src/http/axios-orders.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 const instance = axios.create({
-    baseURL: 'https://burger-builder-f349f.firebaseio.com'
+    // baseURL: 'https://burger-builder-f349f.firebaseio.com'
+    baseURL: 'http://localhost:7070/react/api'
 });
 
 export default instance;
