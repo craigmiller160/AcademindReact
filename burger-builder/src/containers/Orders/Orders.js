5ac753ecceede9eeb8e1125fa6c9c48401eaefcd
craigmiller160 <craigmiller160@gmail.com>
2018-07-16 22:50:30 -0500

    Started fetching orders

--- burger-builder/src/containers/Orders/Orders.js
+++ burger-builder/src/containers/Orders/Orders.js
@@ -1,7 +1,31 @@
 import React, { Component } from 'react';
 import Order from '../../components/Order/Order';
+import axiosOrders from '../../http/axios-orders';
+import withErrorHandler from '../../hoc/withErrorHandler/withErrorHandler';
 
 class Orders extends Component {
+
+    state = {
+        orders: [],
+        loading: true
+    };
+
+    async componentDidMount() {
+        try {
+            const res = await axiosOrders.get('/orders.json');
+            const orders = Object.keys(res.data).map(key => ({
+                id: key,
+                ...res.data[key]
+            }));
+            this.setState({orders});
+        }
+        catch (ex) {
+            console.log(ex);
+        }
+        finally {
+            this.setState({loading: false});
+        }
+    }
 
     render() {
         return (
@@ -14,4 +38,4 @@ class Orders extends Component {
 
 }
 
-export default Orders;
+export default withErrorHandler(Orders, axiosOrders);
--- burger-builder/src/http/axios-orders.js
+++ burger-builder/src/http/axios-orders.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
 const instance = axios.create({
-    // baseURL: 'https://burger-builder-f349f.firebaseio.com'
-    baseURL: 'http://localhost:7070/react/api'
+    baseURL: 'https://burger-builder-f349f.firebaseio.com'
+    // baseURL: 'http://localhost:7070/react/api'
 });
 
 export default instance;
