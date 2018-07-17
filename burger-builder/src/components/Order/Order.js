529f9b6965bb0346bceef4b644de8f4aad32efcc
craigmiller160 <craigmiller160@gmail.com>
2018-07-17 08:17:11 -0500

    Displayed the orders from the server

--- burger-builder/src/components/Order/Order.js
+++ burger-builder/src/components/Order/Order.js
@@ -1,13 +1,33 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import classes from './Order.css';
+import { ingredientsPropType } from '../../model/ingredient/BurgerIngredients';
 
 const Order = props => {
+    const ingredients = Object.keys(props.ingredients)
+        .map(key => ({
+            name: key,
+            amount: props.ingredients[key]
+        }))
+        .map(ingredient => (
+            <span
+                key={ingredient.name}
+                className={classes.ingredient}>
+                {ingredient.name} ({ingredient.amount})
+            </span>
+        ));
+
     return (
         <div className={classes.Order}>
-            <p>Ingredients: Salad (1)</p>
-            <p>Price: <strong>USD 5.45</strong></p>
+            <p>Ingredients: {ingredients}</p>
+            <p>Price: <strong>USD {props.price.toFixed(2)}</strong></p>
         </div>
     );
+};
+
+Order.propTypes = {
+    price: PropTypes.number,
+    ingredients: ingredientsPropType
 };
 
 export default Order;
