681f3f6689ad589a5c0688c609ff89fc11e4ce29
craigmiller160 <craigmiller160@gmail.com>
2018-07-20 18:01:31 -0500

    Added the redux store to the assignment

--- Lesson 14 - Redux/redux-assignment/src/index.js
+++ Lesson 14 - Redux/redux-assignment/src/index.js
@@ -3,6 +3,14 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import { Provider } from 'react-redux';
+import store from './store/store';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const app = (
+    <Provider store={store}>
+        <App />
+    </Provider>
+);
+
+ReactDOM.render(app, document.getElementById('root'));
 registerServiceWorker();
