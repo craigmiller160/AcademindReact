1c99507585093c859b14cffd536ca0ffa255c60b
craigmiller160 <craigmiller160@gmail.com>
2018-07-19 21:15:36 -0500

    Explored using subscriptions

--- Lesson 14 - Redux/learn-redux/redux-basics.js
+++ Lesson 14 - Redux/learn-redux/redux-basics.js
@@ -27,5 +27,11 @@ console.log(store.getState());
 // Dispatching Action
 store.dispatch({type: 'INC_COUNTER'});
 store.dispatch({type: 'ADD_COUNTER', value: 10});
+console.log(store.getState());
 
 // Subscription
+store.subscribe(() => {
+    console.log('[Subscription]: ', store.getState());
+});
+
+store.dispatch({type: 'INC_COUNTER'});
