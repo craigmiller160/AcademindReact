69caaf730d1d9c04e76a762f3e4c0023e3ae24f1
craigmiller160 <craigmiller160@gmail.com>
2018-07-19 20:13:47 -0500

    Added redux and learned some redux basics

---
+++ Lesson 14 - Redux/learn-redux/redux-basics.js
@@ -0,0 +1,31 @@
+const redux = require('redux');
+const createStore = redux.createStore;
+
+const initialState = {
+    counter: 0
+};
+
+// Reducer
+const rootReducer = (state = initialState, action) => {
+    const newState = {...state};
+    switch (action.type) {
+        case 'INC_COUNTER':
+            newState.counter++;
+            break;
+        case 'ADD_COUNTER':
+            newState.counter += action.value;
+            break;
+    }
+
+    return newState;
+};
+
+// Store
+store = createStore(rootReducer);
+console.log(store.getState());
+
+// Dispatching Action
+store.dispatch({type: 'INC_COUNTER'});
+store.dispatch({type: 'ADD_COUNTER', value: 10});
+
+// Subscription
