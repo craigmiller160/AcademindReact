5cc848773830611d5f0f6d69da943eba107f2bde
Craig Miller <craigmiller160@gmail.com>
2018-12-08 17:18:50 -0500

    Enabled redux devtools in burger builder

--- burger-builder/src/store/store.js
+++ burger-builder/src/store/store.js
@@ -1,6 +1,9 @@
 import { createStore } from 'redux';
 import burgerReducer from './modules/burger/burgerReducer';
 
-const store = createStore(burgerReducer);
+const store = createStore(
+    burgerReducer,
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
 
 export default store;
