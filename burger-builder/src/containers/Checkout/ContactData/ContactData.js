9e18e5c5952c8b44739c2a096276f4ac90063b9d
Craig Miller <craigmiller160@gmail.com>
2018-12-09 13:38:27 -0500

    Refactored how the order form is set up in ContactData component

--- burger-builder/src/containers/Checkout/ContactData/ContactData.js
+++ burger-builder/src/containers/Checkout/ContactData/ContactData.js
@@ -6,54 +6,12 @@ import { ingredientsPropType } from '../../../model/ingredient/BurgerIngredients
 import Spinner from '../../../components/UI/Spinner/Spinner';
 import { withRouter } from 'react-router-dom';
 import Input from '../../../components/UI/Form/Input/Input';
-import FormInput from '../../../model/form/FormInput';
-import * as inputTypes from '../../../model/form/InputTypes';
-import SelectOption from '../../../model/form/SelectOption';
+import contactDataOrderForm from './contactDataOrderForm';
 
 class ContactData extends Component {
 
     state = {
-        orderForm: [
-            new FormInput(inputTypes.TEXT, 'name', {
-                label: 'Name',
-                placeholder: 'Your name'
-            }, {
-                required: true
-            }),
-            new FormInput(inputTypes.TEXT, 'street', {
-                label: 'Street',
-                placeholder: 'Your street'
-            }, {
-                required: true
-            }),
-            new FormInput(inputTypes.TEXT, 'zipCode', {
-                label: 'Zip Code',
-                placeholder: 'Your zip code'
-            }, {
-                required: true,
-                minLength: 5,
-                maxLength: 5
-            }),
-            new FormInput(inputTypes.TEXT, 'country', {
-                label: 'Country',
-                placeholder: 'Your country'
-            }, {
-                required: true
-            }),
-            new FormInput(inputTypes.EMAIL, 'email', {
-                label: 'Email',
-                placeholder: 'Your email'
-            }, {
-                required: true
-            }),
-            new FormInput(inputTypes.SELECT, 'deliveryMethod', {
-                label: 'Delivery Method',
-                options: [
-                    new SelectOption('fastest', 'Fastest'),
-                    new SelectOption('cheapest', 'Cheapest')
-                ]
-            })
-        ],
+        orderForm: contactDataOrderForm,
         formIsValid: false
     };
 
@@ -78,7 +36,7 @@ class ContactData extends Component {
         });
     };
 
-    checkValidity(value, rules) {
+    static checkValidity(value, rules) {
         let isValid = true;
 
         if (rules && rules.required) {
